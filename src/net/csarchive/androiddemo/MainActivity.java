696abfdf8c25a0bfcb003bab64d70d4f4b8c77de
Shahryar Naseri <shahryar@csarchive.net>
2014-09-30 01:07:31 +0530

    Modified the MainActivity.java file to include our TextView and Buttons and find them by IDs

--- src/net/csarchive/androiddemo/MainActivity.java
+++ src/net/csarchive/androiddemo/MainActivity.java
@@ -4,14 +4,24 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+	
+	int count;
+	Button add, sub;
+	TextView display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        count = 0;
+        add = (Button) findViewById(R.id.bAdd);
+        sub = (Button) findViewById(R.id.bSub);
+        display = (TextView) findViewById(R.id.tvDisplay);
     }
 
 
