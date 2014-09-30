3c8dbeabfedf4d927f9ca34b4af533b850de941d
Shahryar Naseri <shahryar@csarchive.net>
2014-10-01 01:36:11 +0530

    Added onPuase method to the Splash activity to kill it when MainActivity starts

--- src/net/csarchive/androiddemo/Splash.java
+++ src/net/csarchive/androiddemo/Splash.java
@@ -26,4 +26,11 @@ public class Splash extends Activity{
 		timer.start();
 	}
 
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
+
 }
