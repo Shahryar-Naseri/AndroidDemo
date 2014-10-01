6a5b7f2074fc6863226fdc9d000c94ad5bb22ba9
Shahryar Naseri <shahryar@csarchive.net>
2014-10-01 13:08:12 +0530

    Added a startup song ot the Splash activity using the MediaPlayer method

--- src/net/csarchive/androiddemo/Splash.java
+++ src/net/csarchive/androiddemo/Splash.java
@@ -2,15 +2,20 @@ package net.csarchive.androiddemo;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class Splash extends Activity{
+	
+	MediaPlayer song;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash);
+		song = MediaPlayer.create(Splash.this, R.raw.startup);
+		song.start();
 		Thread timer = new Thread(){
 			public void run(){
 				try{
@@ -30,6 +35,7 @@ public class Splash extends Activity{
 	protected void onPause() {
 		// TODO Auto-generated method stub
 		super.onPause();
+		song.release();
 		finish();
 	}
 
