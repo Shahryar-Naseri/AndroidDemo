022c3873e76b76910e8c0f5430a2ae3ce3f4565f
Shahryar Naseri <shahryar@csarchive.net>
2014-10-05 22:46:25 +0530

    Added reference variables to TextPlay.java class

--- src/net/csarchive/androiddemo/TextPlay.java
+++ src/net/csarchive/androiddemo/TextPlay.java
@@ -2,6 +2,10 @@ package net.csarchive.androiddemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class TextPlay extends Activity{
 
@@ -10,6 +14,10 @@ public class TextPlay extends Activity{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.text);
+		Button btn = (Button) findViewById(R.id.btn1);
+		ToggleButton tbtn = (ToggleButton) findViewById(R.id.tbtn);
+		EditText et = (EditText) findViewById(R.id.et1);
+		TextView display = (TextView) findViewById(R.id.tv1);
 	}
 
 }
