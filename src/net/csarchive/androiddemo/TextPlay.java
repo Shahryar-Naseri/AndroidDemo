b5ced353e3ba0ad6191f90e9b617f04639ea1742
Shahryar Naseri <shahryar@csarchive.net>
2014-10-09 01:37:39 +0530

    Added a onClickListener for try command button in the TextPlay.java file with if else if statements

--- src/net/csarchive/androiddemo/TextPlay.java
+++ src/net/csarchive/androiddemo/TextPlay.java
@@ -2,6 +2,8 @@ package net.csarchive.androiddemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,9 +17,22 @@ public class TextPlay extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.text);
 		Button btn = (Button) findViewById(R.id.btn1);
-		ToggleButton tbtn = (ToggleButton) findViewById(R.id.tbtn);
-		EditText et = (EditText) findViewById(R.id.et1);
+		final ToggleButton tbtn = (ToggleButton) findViewById(R.id.tbtn);
+		final EditText et = (EditText) findViewById(R.id.et1);
 		TextView display = (TextView) findViewById(R.id.tv1);
+		tbtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if(tbtn.isChecked()){
+					et.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+				}
+				else{
+					et.setInputType(InputType.TYPE_CLASS_TEXT);
+				}
+			}
+		});
 	}
 
 }
